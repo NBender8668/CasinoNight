7b4468e2e3f0f56390540b4ff89e2a3346716e34
Spencer <thebuff2001@gmail.com>
2021-05-06 17:47:52 -0600

    Betting UI finished for Poker

--- Assets/Scripts/DeckOfCards.cs
+++ Assets/Scripts/DeckOfCards.cs
@@ -9,21 +9,18 @@ public class DeckOfCards : MonoBehaviour
 
     public DeckOfCards()
     {
-        foreach(Card.eSuit suit in Enum.GetValues(typeof(Card.eSuit)))
+        foreach (Card.eSuit suit in Enum.GetValues(typeof(Card.eSuit)))
         {
-            for(int i = 1; i < 14; i++)
+            for (int i = 1; i < 14; i++)
             {
                 cards.Add(new Card(i, suit));
             }
         }
     }
 
-<<<<<<< HEAD
-   // public void shuffle()
-=======
     public void shuffle()
     {
 
     }
->>>>>>> d3cb323cfc39fbb816784388e0aa15e279b967cf
 }
+
