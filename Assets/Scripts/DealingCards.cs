449a6676fab015ea0462f8a2aa2bcd51d76e32db
Spencer <thebuff2001@gmail.com>
2021-05-12 17:00:21 -0600

    its making me commit

--- Assets/Scripts/DealingCards.cs
+++ Assets/Scripts/DealingCards.cs
@@ -1,18 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DealingCards : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public Button cardOne;
+    public Button cardTwo;
+    public Button cardThree;
+    public Button cardFour;
+    public Button cardFive;
 }
