af3fcc18608e9cb8526659978d01bddfbfba3ca9
Spencer <thebuff2001@gmail.com>
2021-05-12 17:55:20 -0600

    Dealing Cards in progress

--- Assets/Scripts/DealingCards.cs
+++ Assets/Scripts/DealingCards.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,9 +6,51 @@ using UnityEngine.UI;
 
 public class DealingCards : MonoBehaviour
 {
-    public Button cardOne;
-    public Button cardTwo;
-    public Button cardThree;
-    public Button cardFour;
-    public Button cardFive;
+    public Button cardOneBtn;
+    public Button cardTwoBtn;
+    public Button cardThreeBtn;
+    public Button cardFourBtn;
+    public Button cardFiveBtn;
+
+    private DeckOfCards deck;
+
+    private Card cardOne;
+    private Card cardTwo;
+    private Card cardThree;
+    private Card cardFour;
+    private Card cardFive;
+
+
+    public void DealCards()
+    {
+        deck.shuffle();
+        int numOfRandom = 0;
+
+        System.Random randomCard = new System.Random();
+        System.Random randomCard2 = new System.Random();
+        System.Random randomCard3 = new System.Random();
+        System.Random randomCard4 = new System.Random();
+        System.Random randomCard5 = new System.Random();
+
+        int indexForCardOne = randomCard.Next(deck.cards.Count);
+        deck.cards.Remove(deck.cards[indexForCardOne]);
+
+        int indexForCardTwo = randomCard.Next(deck.cards.Count);
+        deck.cards.Remove(deck.cards[indexForCardTwo]);
+
+        int indexForCardThree = randomCard.Next(deck.cards.Count);
+        deck.cards.Remove(deck.cards[indexForCardThree]);
+
+        int indexForCardFour = randomCard.Next(deck.cards.Count);
+        deck.cards.Remove(deck.cards[indexForCardFour]);
+
+        int indexForCardFive = randomCard.Next(deck.cards.Count);
+        deck.cards.Remove(deck.cards[indexForCardFive]);
+
+        cardOne = deck.cards[indexForCardOne];
+        cardTwo = deck.cards[indexForCardTwo];
+        cardThree = deck.cards[indexForCardThree];
+        cardFour = deck.cards[indexForCardFour];
+        cardFive = deck.cards[indexForCardFive];
+    }
 }
