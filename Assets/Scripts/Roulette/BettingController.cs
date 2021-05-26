d4dbb87ca55432dd0c3091c1938b23f7162a7420
Dalton Cobb <DCobb@student.nemount.edu>
2021-05-26 17:42:43 -0600

    Roulette things

---
+++ Assets/Scripts/Roulette/BettingController.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BettingController : MonoBehaviour
+{
+    public int currentMoney = 100;
+    int timesWon = 0;
+    int timesTried = 1000;
+
+    public void CheckMoney()
+    {
+
+    }
+
+ 
+}
