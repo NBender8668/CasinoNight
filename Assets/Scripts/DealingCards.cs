0040e1318239c5c5ae6144e468818bd6866eff12
Spencer <thebuff2001@gmail.com>
2021-05-26 17:08:02 -0600

    Scrapping Poker

--- Assets/Scripts/DealingCards.cs
+++ Assets/Scripts/DealingCards.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class DealingCards
+public class DealingCards : MonoBehaviour
 {
     private Sprite cardFace;
     private Sprite cardFace2;
